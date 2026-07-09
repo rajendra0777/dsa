8aa51bf6d88d9ee58bccee4851958437f907d0ce
Rajendra Pratap <115072670+rajendra0777@users.noreply.github.com>
2026-07-10 00:09:08 +0530

    Time: 3 ms (57.05%), Space: 44.8 MB (89.16%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,45 @@
+/*
+Approach:
+Idea: 
+    - Sort the array for unique elements
+    - include   (add)
+    - backtrack  (go back)
+    - check for the duplicate
+    - exclude (remove)
+
+*/
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ansList = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+
+        Arrays.sort(nums);
+        findSubset(nums, 0, tempList, ansList);
+        return ansList;
+
+    }
+
+    void findSubset(int[] nums, int index, List<Integer> tempList, List<List<Integer>> ansList) {
+        if (index == nums.length) {
+            ansList.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        //1. includ
+        tempList.add(nums[index]);
+        findSubset(nums, index + 1, tempList, ansList);
+        
+        //2. backtrack
+        tempList.remove(tempList.size() - 1);
+       
+         //3. Skip all duplicate elements for the "exclude" decision
+        while(index+1 < nums.length && nums[index]==  nums[index+1]){
+            index++;
+        }
+
+        //4. exclude
+        findSubset(nums, index + 1, tempList, ansList);
+
+        
+    }
+}
